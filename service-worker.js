5aec8114c4e0d03c4283f69ab358f967d221ca80
Pravin2311 <moonspayhq@gmail.com>
2025-07-31 16:42:23 +0530

    download button added

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,28 @@
 const CACHE_NAME = 'teachings-ai-v1';
+
 const urlsToCache = [
   '/',
   '/index.html',
   '/css/style.css',
   '/js/script.js',
   '/js/musicControl.js',
-  '/shapes/index.html',
-  '/shapes/shapes.js',
+  '/manifest.json',
+
+  // HTML Pages
+  '/alphabets.html',
+  '/animals.html',
+  '/birds.html',
+  '/bodyparts.html',
+  '/colors.html',
+  '/fruits.html',
+  '/healthyfoods.html',
+  '/math.html',
+  '/numbers.html',
+  '/publicservice.html',
+  '/vegetables.html',
+  '/vehicles.html',
+
+  // Assets
   '/assets/audio/background_music.mp3',
   '/assets/audio/click.mp3',
   '/assets/images/app_icon_192.png',
@@ -15,12 +31,16 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
