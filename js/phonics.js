4ffc627705838a683e8c433c20c9739594c5a8a5
Pravin2311 <moonspayhq@gmail.com>
2025-08-24 16:44:49 +0530

    letter sound made capital letters

--- js/phonics.js
+++ js/phonics.js
@@ -1,90 +1,91 @@
     // Define your words with: word, image, sound sequence, full word audio
     const words = [
-      { 
-        word: "CAT", 
-        img: "assets/images/animals/cat.png", 
-        sounds: ["c", "a", "t"], 
-        full: "assets/audio/animals/cat.mp3" 
-      },
-      { 
-        word: "COW", 
-        img: "assets/images/animals/cow.png", 
-        sounds: ["c", "o", "w"], 
-        full: "assets/audio/animals/cow.mp3" 
-      },
-      { 
-        word: "DOG", 
-        img: "assets/images/animals/dog.png", 
-        sounds: ["d", "o", "g"], 
-        full: "assets/audio/animals/dog.mp3" 
-      },
-      { 
-        word: "OWL", 
-        img: "assets/images/birds/owl.png", 
-        sounds: ["o", "w", "l"], 
-        full: "assets/audio/birds/owl.mp3" 
-      },
-      { 
-        word: "BUS", 
-        img: "assets/images/vehicles/bus.png", 
-        sounds: ["b", "u", "s"], 
-        full: "assets/audio/vehicles/bus.mp3" 
-      },
-      { 
-        word: "CAR", 
-        img: "assets/images/vehicles/car.png", 
-        sounds: ["c", "a", "r"], 
-        full: "assets/audio/vehicles/car.mp3" 
-      },
-      { 
-        word: "HORSE", 
-        img: "assets/images/animals/horse.png", 
-        sounds: ["h", "o", "r", "s", "e"], 
-        full: "assets/audio/animals/horse.mp3" 
-      },
-      { 
-        word: "HEN", 
-        img: "assets/images/birds/hen.png", 
-        sounds: ["h", "e", "n"], 
-        full: "assets/audio/birds/hen.mp3" 
-      },
-      { 
-        word: "LION", 
-        img: "assets/images/animals/lion.png", 
-        sounds: ["l", "i", "o", "n"], 
-        full: "assets/audio/animals/lion.mp3" 
-      },
-      { 
-        word: "TIGER", 
-        img: "assets/images/animals/tiger.png", 
-        sounds: ["t", "i", "g", "e", "r"], 
-        full: "assets/audio/animals/tiger.mp3" 
-      },
-      { 
-        word: "DUCK", 
-        img: "assets/images/birds/duck.png", 
-        sounds: ["d", "u", "c", "k"], 
-        full: "assets/audio/birds/duck.mp3" 
-      },
-      { 
-        word: "APPLE", 
-        img: "assets/images/fruits/apple.png", 
-        sounds: ["a", "p", "p", "l", "e"], 
-        full: "assets/audio/fruits/apple.mp3" 
-      },
-      { 
-        word: "EARTH", 
-        img: "assets/images/planets/earth.png", 
-        sounds: ["e", "a", "r", "t", "h"], 
-        full: "assets/audio/planets/earth.mp3" 
-      },
-      { 
-        word: "MANGO", 
-        img: "assets/images/fruits/mango.png", 
-        sounds: ["m", "a", "n", "g", "o"], 
-        full: "assets/audio/fruits/mango.mp3" 
-      }
-    ];
+  { 
+    word: "CAT", 
+    img: "assets/images/animals/cat.png", 
+    sounds: ["C", "A", "T"], 
+    full: "assets/audio/animals/cat.mp3" 
+  },
+  { 
+    word: "COW", 
+    img: "assets/images/animals/cow.png", 
+    sounds: ["C", "O", "W"], 
+    full: "assets/audio/animals/cow.mp3" 
+  },
+  { 
+    word: "DOG", 
+    img: "assets/images/animals/dog.png", 
+    sounds: ["D", "O", "G"], 
+    full: "assets/audio/animals/dog.mp3" 
+  },
+  { 
+    word: "OWL", 
+    img: "assets/images/birds/owl.png", 
+    sounds: ["O", "W", "L"], 
+    full: "assets/audio/birds/owl.mp3" 
+  },
+  { 
+    word: "BUS", 
+    img: "assets/images/vehicles/bus.png", 
+    sounds: ["B", "U", "S"], 
+    full: "assets/audio/vehicles/bus.mp3" 
+  },
+  { 
+    word: "CAR", 
+    img: "assets/images/vehicles/car.png", 
+    sounds: ["C", "A", "R"], 
+    full: "assets/audio/vehicles/car.mp3" 
+  },
+  { 
+    word: "HORSE", 
+    img: "assets/images/animals/horse.png", 
+    sounds: ["H", "O", "R", "S", "E"], 
+    full: "assets/audio/animals/horse.mp3" 
+  },
+  { 
+    word: "HEN", 
+    img: "assets/images/birds/hen.png", 
+    sounds: ["H", "E", "N"], 
+    full: "assets/audio/birds/hen.mp3" 
+  },
+  { 
+    word: "LION", 
+    img: "assets/images/animals/lion.png", 
+    sounds: ["L", "I", "O", "N"], 
+    full: "assets/audio/animals/lion.mp3" 
+  },
+  { 
+    word: "TIGER", 
+    img: "assets/images/animals/tiger.png", 
+    sounds: ["T", "I", "G", "E", "R"], 
+    full: "assets/audio/animals/tiger.mp3" 
+  },
+  { 
+    word: "DUCK", 
+    img: "assets/images/birds/duck.png", 
+    sounds: ["D", "U", "C", "K"], 
+    full: "assets/audio/birds/duck.mp3" 
+  },
+  { 
+    word: "APPLE", 
+    img: "assets/images/fruits/apple.png", 
+    sounds: ["A", "P", "P", "L", "E"], 
+    full: "assets/audio/fruits/apple.mp3" 
+  },
+  { 
+    word: "EARTH", 
+    img: "assets/images/planets/earth.png", 
+    sounds: ["E", "A", "R", "T", "H"], 
+    full: "assets/audio/planets/earth.mp3" 
+  },
+  { 
+    word: "MANGO", 
+    img: "assets/images/fruits/mango.png", 
+    sounds: ["M", "A", "N", "G", "O"], 
+    full: "assets/audio/fruits/mango.mp3" 
+  }
+];
+
 
     let currentIndex = 0;
 
